5dfd3db7e442abdd72e804888961b034f868af9d
hiroki <hiroki.osame@gmail.com>
2021-01-10 19:20:34 -0500

    perf: terser to mangle (#19)

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 import builtins from 'builtin-modules';
 
 const rollupConfig = {
@@ -8,6 +9,12 @@ const rollupConfig = {
 		commonjs(),
 		nodeResolve({
 			preferBuiltins: false,
+		}),
+		terser({
+			compress: false,
+			format: {
+				beautify: true,
+			},
 		}),
 	],
 	external: builtins,
