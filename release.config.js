f8670a5ec5d9cc55ddc96211fd632fc29ff6abf9
Hiroki Osame <hiroki.osame@gmail.com>
2021-08-02 13:34:25 -0400

    ci(release): use conventional commits and disable npm publish

--- release.config.js
+++ release.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-	plugins: [
-		'@semantic-release/commit-analyzer',
-		'@semantic-release/release-notes-generator',
-		'@semantic-release/github',
-	],
+	preset: 'conventionalcommits',
+	npmPublish: false,
 };
