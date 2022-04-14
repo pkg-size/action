330981f89963589aadd5b9860d8cd6c5031672bc
Alexandros Dorodoulis <aldor94@gmail.com>
2022-04-14 10:33:55 +0200

    feat: make install command configurable

--- src/lib/npm-ci.js
+++ src/lib/npm-ci.js
@@ -4,7 +4,10 @@ import { rmRF } from '@actions/io';
 import * as log from './log.js';
 import exec from './exec.js';
 
-async function npmCi({ cwd } = {}) {
+async function npmCi({
+	cwd,
+	installCommand = autoDetectInstallCommand(),
+} = {}) {
 	if (fs.existsSync('node_modules')) {
 		log.info('Cleaning node_modules');
 		await rmRF(path.join(cwd, 'node_modules'));
@@ -15,30 +18,32 @@ async function npmCi({ cwd } = {}) {
 		ignoreReturnCode: true,
 	};
 
-	let installCommand = '';
-
-	if (fs.existsSync('package-lock.json')) {
-		log.info('Installing dependencies with npm');
-		installCommand = 'npm ci';
-	} else if (fs.existsSync('yarn.lock')) {
-		log.info('Installing dependencies with yarn');
-
-		// yarn is installed on GitHub Actions by default
-		installCommand = 'yarn install --frozen-lockfile';
-	} else if (fs.existsSync('pnpm-lock.yaml')) {
-		log.info('Installing dependencies with pnpm');
-
-		// pnpm is not installed on GitHub Actions by default
-		installCommand = 'npx pnpm i --frozen-lockfile';
-	} else {
-		log.info('No lock file detected. Installing dependencies with npm');
-		installCommand = 'npm i';
-	}
-
 	const { exitCode, stdout, stderr } = await exec(installCommand, options);
 	if (exitCode > 0) {
 		throw new Error(`${stderr}\n${stdout}`);
 	}
+}
+
+function autoDetectInstallCommand() {
+	if (fs.existsSync('package-lock.json')) {
+		log.info('Installing dependencies with npm');
+		return 'npm ci';
+	}
+
+	if (fs.existsSync('yarn.lock')) {
+		log.info('Installing dependencies with yarn');
+		// yarn is installed on GitHub Actions by default
+		return 'yarn install --frozen-lockfile';
+	}
+
+	if (fs.existsSync('pnpm-lock.yaml')) {
+		log.info('Installing dependencies with pnpm');
+		// pnpm is not installed on GitHub Actions by default
+		return 'npx pnpm i --frozen-lockfile';
+	}
+
+	log.info('No lock file detected. Installing dependencies with npm');
+	return 'npm i';
 }
 
 export default npmCi;
